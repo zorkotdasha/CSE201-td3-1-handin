84ae01d9cb4d7255949098c91071be966e23bca7
darya-todoskova <zorkotdasha@gmail.com>
2021-09-25 10:06:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -36,6 +36,7 @@ int main(int argc, char* argv[])
 
   {
         std::cout << "A better, new initial message" << std::endl;
+        std::cout << "Hi" << std::endl;
   }
   // END OF THE CUSTOM CODE SECTION
 #else
